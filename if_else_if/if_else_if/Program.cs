675013a470f693e3607c20c53440fd0dadddcdb3
Emre Değirmenci <59510052+Emreddegirmenci@users.noreply.github.com>
2021-11-14 13:20:00 +0300

    Add files via upload

---
+++ if_else_if/if_else_if/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+            string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "İyi Günleriniz olsun" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
