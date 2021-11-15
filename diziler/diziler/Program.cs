35bbe6b7ef60c716a199101b8f35072e49dbd214
Emre Değirmenci <59510052+Emreddegirmenci@users.noreply.github.com>
2021-11-15 20:24:34 +0300

    Add files via upload

---
+++ diziler/diziler/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dizi Tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+
+            // Dizilere Değer Atama ve Erişim
+            renkler[0] = "Mavi";
+            
+
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+
+            // Döngülerle Dizi Kullanımı
+
+            // Klavyeden girilen n tane sayının
+            // ortalamasını alan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz : ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}.sayıyı giriniz : "  , i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+                
+            }
+            Console.Write("Ortalama :" + toplam / diziUzunlugu);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
